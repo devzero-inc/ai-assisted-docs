d92d1cc280052ba1a1367eacc406bc5ded17214e
Adosh Singh <adoshbhanoo@gmail.com>
2024-03-26 16:19:26 +0530

    #02 AS: Implemented UI and configured the app to use supabase locally

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,13 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-gradient': 'linear-gradient(to right, #5d9fde, #7866ef, #d362f0)',
+        'custom-gradient-2': 'linear-gradient(to right, #e73c9f, #9e32c7, #4025f9)',
       },
+      colors:{
+        'primary': '#0f0b29',
+        'secondary': '#261046',
+      }
     },
   },
   plugins: [],
